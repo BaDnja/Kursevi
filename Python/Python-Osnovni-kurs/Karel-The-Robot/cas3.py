a0825b87788e2e1c4697b72a7736a2b3e470a5bb
BaDnja <badnja25@gmail.com>
2021-03-01 22:25:17 +0100

    cas3 i dodatni zadaci

--- Python/Python-Osnovni-kurs/Karel-The-Robot/cas3.py
+++ Python/Python-Osnovni-kurs/Karel-The-Robot/cas3.py
@@ -9,3 +9,141 @@ Treći čas - Vježbanje
 - Grananje
 - Ugnježdene petlje
 """
+
+# PETLJA FOR
+# Prebaci pet loptica u rupu
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id2
+'''
+from karel import *
+napred()
+for i in range(5):
+    uzmi()
+napred()
+for i in range(5):
+    ostavi()
+'''
+
+# Pokupi loptice sa naredna tri polja
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id5
+'''
+from karel import *
+napred()
+for i in range(5):
+    uzmi()
+napred()
+for i in range(3):
+    uzmi()
+napred()
+for i in range(8):
+    uzmi()
+'''
+
+# Pobjedničko postolje
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id6
+'''
+from karel import *
+for i in range(5):
+    levo()
+    napred()
+    desno()
+    napred()
+    uzmi()
+for i in range(5):
+    napred()
+    desno()
+    napred()
+    uzmi()
+    levo()
+'''
+
+# PETLJA WHILE
+# Stepenice
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id9
+'''
+from karel import *
+while moze_napred():
+    napred()
+    desno()
+    napred()
+    uzmi()
+    levo()
+'''
+
+# GRANANJE
+# Donesi sve loptice
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id13
+'''
+from karel import *
+while moze_napred():
+    napred()
+    if ima_loptica_na_polju():
+        uzmi()
+
+levo(); levo()                # okreni se nazad
+while moze_napred():
+    napred()
+
+while ima_loptica_kod_sebe(): # ostavi sve loptice
+    ostavi()
+'''
+
+# UGNJEŽDENE PETLJE
+# Premjesti sve loptice u rupe (3x3)
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#x3
+'''
+from karel import *
+while moze_napred():
+    napred()
+    while ima_loptica_na_polju():
+        uzmi()
+    napred()
+    while ima_loptica_kod_sebe():
+        ostavi()
+'''
+
+# Prebaci sve loptice u rupe
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id17
+'''
+from karel import *
+while moze_napred():
+    napred()
+    while ima_loptica_na_polju():
+        uzmi()
+    napred()
+    while ima_loptica_kod_sebe():
+        ostavi()
+'''
+
+# Uzimaj po četiri loptice do kraja
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id20
+'''
+from karel import *
+while moze_napred():
+    napred()
+    while ima_loptica_na_polju():
+        uzmi()
+'''
+
+# Pokupi sve loptice
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id21
+'''
+from karel import *
+while moze_napred():
+    napred()
+    while ima_loptica_na_polju():
+        uzmi()
+'''
+
+# Pun lavirint loptica
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-cas3#id22
+'''
+from karel import *
+from karel import *
+for i in range(4):
+    while moze_napred():
+        napred()
+        ostavi()
+    desno()
+'''
+
+
--- Python/Python-Osnovni-kurs/Karel-The-Robot/dodatni-zadaci.py
+++ Python/Python-Osnovni-kurs/Karel-The-Robot/dodatni-zadaci.py
@@ -4,3 +4,138 @@ https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci
 
 Dodatni zadaci
 """
+
+# ZADACI
+# Pomjeri sve loptice unazad
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id3
+'''
+from karel import *
+while moze_napred():             # dok ima polja ispred Karela, ponavljaj
+    napred();                        # idi napred
+    while ima_loptica_na_polju():    # pokupi sve loptice sa polja
+        uzmi()
+    if ima_loptica_kod_sebe():
+    	levo(); levo(); napred()        # idi jedno polje nazad
+    while ima_loptica_kod_sebe():    # ostavi sve loptice
+        ostavi()
+    if ima_loptica_na_polju():
+    	levo(); levo(); napred()         # vrati se jedno polje napred
+'''
+
+# Tri puta gore-dole
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id4
+'''
+from karel import *
+for i in range(3):              # tri puta ponovi sve sto sledi
+    napred(); levo()             #    udji u sledecu kolonu i okreni se na sever
+    for i in range(4):
+        napred()
+    desno(); napred(); desno()   #    predji u sledecu kolonu i okreni se na jug
+    for i in range(4):
+        napred()
+    levo()                       #    okreni se na istok
+'''
+
+# Gore-dole
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id5
+'''
+from karel import *
+while moze_napred():
+    napred(); levo()
+    while moze_napred():
+        napred()
+    desno(); napred(); desno()
+    while moze_napred():
+        napred()
+    levo()
+'''
+
+# Donesi sve sa table
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id6
+'''
+from karel import *
+for i_kolona in range(4):      # cetiri puta ponovi ciscenje kolone
+    napred()                   #     udji u sledecu kolonu
+    levo()                     #     okreni se na sever
+    for i in range(3):
+        napred(); uzmi(); #     3 puta ponovi korak napred i uzimanje
+
+    desno(); desno()           #     okreni se na jug
+    for i in range(3):
+        napred() #     3 koraka napred do donje ivice
+
+    levo()                     #     okreni se na istok
+    
+                               # sada smo prosli sva polja
+levo()                         #     okreni se na zapad
+levo()
+for i in range(4):
+    napred() # vrati se na pocetno polje
+    
+for i_loptica in range(12):
+    ostavi()
+'''
+
+# Donesi svih 60
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id7
+'''
+from karel import *
+for i_kolona in range(4):      # cetiri puta ponovi ciscenje kolone
+    napred()                   #     udji u sledecu kolonu
+    levo()                     #     okreni se na sever
+    for i in range(3):
+        napred()
+        for j in range(5):
+            uzmi() #     3 puta ponovi korak napred i uzimanje
+
+    desno(); desno()           #     okreni se na jug
+    for i in range(3):
+        napred() #     3 koraka napred do donje ivice
+
+    levo()                     #     okreni se na istok
+    
+                               # sada smo prosli sva polja
+levo()                         #     okreni se na zapad
+levo()
+for i in range(4):
+    napred() # vrati se na pocetno polje
+    
+for i_loptica in range(60):
+    ostavi()
+'''
+
+# Sakupi loptice na stepenicama
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id8
+'''
+from karel import *
+for i in range(3):
+    levo(); napred();
+    while ima_loptica_na_polju():
+        uzmi()
+    desno(); napred();
+    while ima_loptica_na_polju():
+        uzmi()
+for i in range(3):
+    desno(); napred();
+    while ima_loptica_na_polju():
+        uzmi()
+    levo(); napred();
+    while ima_loptica_na_polju():
+        uzmi()
+'''
+
+# Prepone
+# https://petlja.org/biblioteka/r/lekcije/prirucnik-python/karel-karelzadaci#id9
+'''
+from karel import *
+while not ima_loptica_kod_sebe():
+    levo()
+    while moze_napred():
+        napred()
+    desno(); napred(); desno();
+    while moze_napred():
+        napred()
+    if ima_loptica_na_polju():
+        uzmi()
+    levo()
+'''
