fbf6862696b8af0adfc7cf87836bbc5f96d258c9
Amar Badnjevic <abadnjevic@planetsoft.ba>
2021-04-28 13:21:02 +0200

    izracunavanje - cas 7 zavrsen

---
+++ Python/Python-Osnovni-kurs/Izracunavanje/1-Osnovne aritmeticke operacije i primjena/domaci-zadaci/rjesenja.py
@@ -0,0 +1,28 @@
+
+'''
+# Trkači
+sinisa = 2347
+nenad = sinisa + 987
+milos = 2 * nenad
+ukupno = sinisa + nenad + milos
+print(ukupno)
+'''
+
+'''
+# Tepih
+dimenzija_sobe = int(input("Unesi dimenziju sobe: "))
+udaljenost_tepiha = int(input("Unesi udaljenost tepiha od zida: "))
+cijena_pranja_po_m2 = int(input("Unesi cijenu pranja kvadratnog metra tepiha: "))
+dimenzija_tepiha = dimenzija_sobe - (udaljenost_tepiha*2)
+ukupna_cijena_pranja = dimenzija_tepiha**2 * cijena_pranja_po_m2
+print(ukupna_cijena_pranja)
+'''
+
+'''
+# Fudbaleri i košarkaši
+ukupno_ucenika = int(input("Unesi ukupan broj ucenika: "))
+fudbal = int(input("Unesi broj ucenika koji treniraju fudbal: "))
+kosarka = int(input("Unesi broj ucenika koji treniraju kosarku: "))
+dva_sporta = (fudbal + kosarka) - ukupno_ucenika
+print(dva_sporta)
+'''
